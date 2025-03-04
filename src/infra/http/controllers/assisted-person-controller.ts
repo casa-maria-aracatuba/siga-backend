ff755b9238b1dd06c873ff27c36ccd1a11c2ad92
thaisminas <thaisminasds@gmail.com>
2025-03-04 16:16:37 -0300

    feat: add new route to delete, update and find assisted people

--- src/infra/http/controllers/assisted-person-controller.ts
+++ src/infra/http/controllers/assisted-person-controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  ParseIntPipe,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { AssistedPersonDto } from '../../../application/dtos/assisted-person-dto';
 import { AssistedPersonService } from '../../../domain/service/assisted-person-service';
 
@@ -11,26 +20,26 @@ export class AssistedPersonController {
     return this.assistedPeopleService.create(createAssistedPersonDto);
   }
 
-  // @Get()
-  // async findAll() {
-  //   return this.assistedPeopleService.findAll();
-  // }
-  //
-  // @Get(':id')
-  // async findOne(@Param('id', ParseIntPipe) id: number) {
-  //   return this.assistedPeopleService.findOne(id);
-  // }
-  //
-  // @Put(':id')
-  // async update(
-  //   @Param('id', ParseIntPipe) id: number,
-  //   @Body() updateAssistedPersonDto: UpdateAssistedPersonDto,
-  // ) {
-  //   return this.assistedPeopleService.update(id, updateAssistedPersonDto);
-  // }
-  //
-  // @Delete(':id')
-  // async remove(@Param('id', ParseIntPipe) id: number) {
-  //   return this.assistedPeopleService.remove(id);
-  // }
+  @Get()
+  async findAll() {
+    return this.assistedPeopleService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id', ParseIntPipe) id: number) {
+    return this.assistedPeopleService.findById(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() assistedPersonDto: AssistedPersonDto,
+  ) {
+    return this.assistedPeopleService.update(id, assistedPersonDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return this.assistedPeopleService.delete(id);
+  }
 }
